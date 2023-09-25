7cd887baf589d7150435b8e7050fe2662a8ee8ec
bonface murangiri <bonnyrangi95@gmail.com>
2023-09-25 13:21:02 +0000

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ *swap_func - swaps two integers
+ * @a: 1st int to swap
+ * @b: 2nd int to swap
+ */
+
+void swap_func(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
   * selection_sort -  sorts an array of integers in ascending order using the
   * Selection sort algorithm
   * @array: pinter to array to sort.
@@ -9,25 +24,21 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int temp;
+	int *index;
+	size_t i, j;
 
 	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
+		index = array + i;
 		for (j = i + 1; j < size; j++)
+			index = (array[j] < *index) ? (array + j) : index;
+		if ((array + i) != index)
 		{
-			if (array[j] < array[min])
-				min = j;
+			swap_func(array + i, index);
+			print_array(array, size);
 		}
-		if (i != min)
-		{
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-		}
-		print_array(array, size);
 	}
 }
